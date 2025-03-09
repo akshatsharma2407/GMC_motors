cabcd04d0b28be33615c0824ef4f4acca36e4f52
Akshat Sharma <akshatsharma5877@gmail.com>
2025-03-09 11:10:33 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Practising mlops with GMC motors.',
+    author='Akshat',
+    license='MIT',
+)
