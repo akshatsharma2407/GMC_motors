15e70db867150559e19927070d7c25d211bde59f
Akshat Sharma <akshatsharma5877@gmail.com>
2025-03-16 11:08:35 +0530

    fix linting issue in model_evaluation

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -1,5 +1,4 @@
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-import numpy as np
 import pandas as pd
 import pickle
 import json
@@ -122,8 +121,8 @@ def main() -> None:
             test_df = load_data("data/processed/test_processed_df.csv")
             pipe = load_pipe(
                 "models/pipe.pkl"
-            )  
-            # loaded only for exp tracking purpose, 
+            )
+            # loaded only for exp tracking purpose,
             # so that we can directly push the model and pipe to production
 
             metrics_dict = predict(test_df, model)
