fc5d65c7587cbc885b93b0f0e4b4fb22e7c53abb
Akshat Sharma <akshatsharma5877@gmail.com>
2025-03-16 14:29:35 +0530

    model signature added in model_evaluation.py

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -1,3 +1,4 @@
+import mlflow.models
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import pandas as pd
 import pickle
@@ -9,6 +10,7 @@ import mlflow
 import dagshub
 from sklearn.pipeline import Pipeline
 import mlflow.sklearn
+import numpy as np
 
 logger = logging.getLogger(os.path.basename(__file__))
 logger.setLevel("DEBUG")
@@ -133,7 +135,17 @@ def main() -> None:
 
             save_metrics("reports/metrics.json", metrics_dict)
 
-            mlflow.sklearn.log_model(full_pipeline, "transformer+RF_regressor_model")
+            data_for_signature = pd.read_csv('C:/Users/aksha/OneDrive/Desktop/GMC_MLOPS/data/raw/GMC_DATA.csv').head(1)
+            data_for_signature["AGE OF CAR"] = data_for_signature["AGE OF CAR"].astype(str)
+            data_for_signature["MODEL"] = data_for_signature["MODEL"].astype(str)
+            data_for_signature.drop(columns='PRICE($)',inplace=True)
+            print(data_for_signature)
+
+            signature = mlflow.models.infer_signature(
+                data_for_signature,full_pipeline.predict(data_for_signature)
+            )
+
+            mlflow.sklearn.log_model(full_pipeline, "transformer+RF_regressor_model",signature=signature)
             mlflow.log_metrics(metrics_dict)
             save_model_info(
                 run.info.run_id,
