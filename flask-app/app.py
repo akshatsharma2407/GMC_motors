787640cc6a7ecce482879716dab2d340e1224e8c
Akshat Sharma <akshatsharma5877@gmail.com>
2025-03-16 15:32:10 +0530

    predict page added

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,9 +1,56 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import mlflow
+from mlflow.tracking import MlflowClient
+import dagshub
+import pandas as pd
 
 app = Flask(__name__)
+
+dagshub.init(repo_owner="akshatsharma2407", repo_name="GMC_motors", mlflow=True)
+
+mlflow.set_tracking_uri("https://dagshub.com/akshatsharma2407/GMC_motors.mlflow")
+
+# load model from model registry
+def get_latest_model_version(model_name):
+    client = MlflowClient()
+    latest_version = client.get_latest_versions(model_name,stages=['Production'])
+
+    if not latest_version: 
+        raise ValueError(f"No model version found for {model_name} in 'Production' stage.")
+    
+    return latest_version[0].version
+
+model_name =  'GMC_MODEL'
+version = get_latest_model_version(model_name)
+model_uri = f"models:/{model_name}/{version}"
+model = mlflow.pyfunc.load_model(model_uri)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+        input_data = {
+            'CAR NAME': [request.form['car_name']],
+            'MODEL/CLASS': [request.form['model_class']],
+            'MODEL': [request.form['model']],
+            'STOCK TYPE': [request.form['stock_type']],
+            'MILEAGE': [int(request.form['mileage'])],
+            'AGE OF CAR': [request.form['age_of_car']],
+            'RATING': [float(request.form['rating'])],
+            'REVIEW': [float(request.form['review'])],
+            'DEALER NAME': [request.form['dealer_name']],
+            'DEALER LOCATION (CITY)': [request.form['dealer_city']],
+            'DEALER LOCATION (STATE)': [request.form['dealer_state']]
+        }
+
+        # Convert to DataFrame
+        input_df = pd.DataFrame(input_data)
+
+        # Make prediction
+        prediction = model.predict(input_df)
+
+        return str(int(prediction[0]))
 
 app.run(debug=True)
