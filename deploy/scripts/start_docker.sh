1bc3e4bcf39e363395dd7393fa7ea31ef64fa93a
Akshat Sharma <akshatsharma5877@gmail.com>
2025-03-25 16:46:48 +0530

    codedeploy with blue-green deployment strategy

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -2,7 +2,7 @@
 
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 851725541946.dkr.ecr.ap-south-1.amazonaws.com
 
-docker pull 851725541946.dkr.ecr.ap-south-1.amazonaws.com/gmcdocker:v18
+docker pull 851725541946.dkr.ecr.ap-south-1.amazonaws.com/gmcdocker:v15
 
 if [ "$(docker ps -q -f name=my-app)" ]; then
     docker stop my-app
@@ -12,4 +12,4 @@ if [ "$(docker ps -aq -f name=my-app)" ]; then
     docker rm my-app || true
 fi 
 
-sudo docker run -d -p 80:5000 -e DAGSHUB_TOKEN=ea3e52cfe4a018dc25e349377aaf17a193a5bc60 --name my-app 851725541946.dkr.ecr.ap-south-1.amazonaws.com/gmcdocker:v18
+sudo docker run -d -p 80:5000 -e DAGSHUB_TOKEN=ea3e52cfe4a018dc25e349377aaf17a193a5bc60 --name my-app 851725541946.dkr.ecr.ap-south-1.amazonaws.com/gmcdocker:v15
